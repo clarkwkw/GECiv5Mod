4fff8b30a6a06615e4037230217953e168846316
clarkwkw <clarkwkw@yahoo.com.hk>
2018-08-19 20:42:16 +0800

    add: religions for city states

--- Civ5Playground/Lua/ScenarioSetup/ReligionSetup.lua
+++ Civ5Playground/Lua/ScenarioSetup/ReligionSetup.lua
@@ -1,27 +1,37 @@
 SetupReligion = function(scenarioId)
 	local civToPlayer = CreateCivToPlayerLookupTable()
+	local minorCivToPlayer = CreateMinorCivToPlayerLookupTable()
 	print("Setting holy cities..")
 	-- Set holy cities
 	for row in GameInfo.ScenarioReligion{ScenarioId = scenarioId} do
-		local player = civToPlayer[GameInfo.Civilizations[row.FounderCivId].ID]
-		local city = GetCityByPlayerAndCityName(player, row.HolyCityName)
-		if city ~= nil then
-			Game.FoundReligion(
-				player:GetID(),
-				GameInfo.Religions[row.ReligionId].ID,
-				GameInfo.Beliefs[row.PantheonBelief].ID,
-				GameInfo.Beliefs[row.FounderBelief1].ID,
-				GameInfo.Beliefs[row.FounderBelief2].ID,
-				GameInfo.Beliefs[row.FounderBelief3].ID,
-				GameInfo.Beliefs[row.FounderBelief4].ID,
-				city
-			)
+		local player = nil
+
+		if GameInfo.Civilizations[row.FounderCivId] ~= nil then
+			player = civToPlayer[GameInfo.Civilizations[row.FounderCivId].ID]
+		elseif GameInfo["MinorCivilizations"][row.FounderCivId] ~= nil then
+			player = minorCivToPlayer[GameInfo["MinorCivilizations"][row.FounderCivId].ID]
+		end
+
+		if player ~= nil then
+			local city = GetCityByPlayerAndCityName(player, row.HolyCityName)
+			if city ~= nil then
+				Game.FoundReligion(
+					player:GetID(),
+					GameInfo.Religions[row.ReligionId].ID,
+					nil,
+					GameInfo.Beliefs[row.FounderBelief1].ID,
+					GameInfo.Beliefs[row.FounderBelief2].ID,
+					GameInfo.Beliefs[row.FounderBelief3].ID,
+					GameInfo.Beliefs[row.FounderBelief4].ID,
+					city
+				)
+			end
 		end
 	end
 	print("Setting follower cities..")
 	--- Change belief
 	for row in GameInfo.ScenarioCityReligion{ScenarioId = scenarioId} do
-		local player = civToPlayer[GameInfo.Civilizations[row.CityCivId].ID]
+		local player = civToPlayer[GameInfo.Civilizations[row.CityCivId].ID] or minorCivToPlayer[GameInfo["MinorCivilizations"][row.FounderCivId].ID]
 		local city = GetCityByPlayerAndCityName(player, row.CityName)
 		if city ~= nil then
 			city:AdoptReligionFully(GameInfo.Religions[row.ReligionId].ID)
@@ -33,6 +43,14 @@ CreateCivToPlayerLookupTable = function()
 	local table = {}
 	for key, player in pairs(Players) do
 		table[player:GetCivilizationType()] = player
+	end
+	return table
+end
+
+CreateMinorCivToPlayerLookupTable = function()
+	local table = {}
+	for key, player in pairs(Players) do
+		table[player:GetMinorCivType()] = player
 	end
 	return table
 end
